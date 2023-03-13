1e13e96fef66df3d77bf5e6f916097e74f2b37de
Rim <bvt.vanthang2010@gmail.com>
2023-03-13 20:21:37 +0700

    generate css

--- resources/functions/assets.php
+++ resources/functions/assets.php
@@ -28,3 +28,40 @@ if ( ! function_exists( 'goza_load_fonts' ) ) {
 
 add_action( 'admin_enqueue_scripts', 'goza_load_fonts' );
 
+
+function pearl_update_custom_styles()
+{
+
+	global $wp_filesystem;
+
+	if (empty($wp_filesystem)) {
+		require_once ABSPATH . '/wp-admin/includes/file.php';
+		WP_Filesystem();
+	}
+
+	/*Generate custom css*/
+	$custom_css = '';
+	//Disable theme HB styles if HB plugin enabled
+
+	$custom_css .= 'a{color: blue;}';
+	
+
+	/*Create dir or update*/
+	$upload_dir = wp_upload_dir();
+
+	if (!$wp_filesystem->is_dir($upload_dir['basedir'] . '/stm_uploads')) {
+		wp_mkdir_p($upload_dir['basedir'] . '/stm_uploads');
+	}
+
+	$custom_style_file = $upload_dir['basedir'] . '/stm_uploads/skin-custom.css';
+
+	/*Update V*/
+	$current_v = get_option('stm_custom_styles_v', 1) + 1;
+	update_option('stm_custom_styles_v', $current_v);
+
+	$wp_filesystem->put_contents($custom_style_file, $custom_css, FS_CHMOD_FILE);
+
+}
+
+pearl_update_custom_styles();
+
