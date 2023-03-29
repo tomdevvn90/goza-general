ea269fadaf3007205f493b84d14f395395493fde
tomdevvn90 <tomdevvn90@gmail.com>
2023-03-29 22:40:13 +0700

    update icon box block

--- resources/blocks/be-icon-box/init.php
+++ resources/blocks/be-icon-box/init.php
@@ -4,44 +4,98 @@ function be_item_icon_box($block){
 
     // ACF field variables
     $heading = get_field('heading_icon_box');
-    $content = get_field('content_icon_box');
+    $link    = get_field('link_icon_box');
     $icon    = (!empty(get_field('icon__icon_box'))) ? get_field('icon__icon_box') : get_template_directory_uri(). '/resources/assets/images/icon-box-default.svg' ;
     $button  = get_field('button_icon_box');
    
     
     switch ($is_style) {
         case "is-style-2":
-            be_template_icon_box_style_2($heading, $content, $icon, $button);
+            be_template_icon_box_style_2($heading, $icon, $button, $link);
             break;
 
         case "is-style-3":
-            be_template_icon_box_style_3($heading, $content, $icon, $button);
+            be_template_icon_box_style_3($heading, $icon, $button, $link);
             break; 
 
         default:
-            be_template_icon_box_default($heading, $content, $icon, $button);
+            be_template_icon_box_default($heading, $icon, $button, $link);
             break; 
     } 
 
 }
 
-function be_template_icon_box_style_3($heading, $content, $icon, $button){
+function be_template_icon_box_style_3($heading, $icon, $button, $link){ ?>
+    <div class="be-icon-box-block-inner--content"> 
+        <div class="be-icon-box-block--icon"> 
+            <img src="<?php echo $icon ?>" alt="icon">
+        </div>
 
-}
+        <?php if(!empty($heading)): ?>
+            <h3 class="be-icon-box-block--heading"> 
+                <?php if(!empty($link)){ ?>
+                    <a href="<?php echo $link ?>">  <?php echo $heading ?>  </a>
+                <?php }else{
+                    echo $heading;
+                } ?>
+            </h3>
+        <?php endif; ?>  
+    </div>
+<?php }
 
-function be_template_icon_box_style_2($heading, $content, $icon, $button){
-
-}
-
-function be_template_icon_box_default($heading, $content, $icon, $button){ ?>
+function be_template_icon_box_style_2($heading, $icon, $button, $link){ ?>
     <div class="be-icon-box-block-warp"> 
-            <div class="be-icon-box-block--icon"> 
-                <img src="<?php echo $icon ?>" alt="icon">
-            </div>
+        <div class="be-icon-box-block--icon"> 
+            <img src="<?php echo $icon ?>" alt="icon">
+        </div>
 
+        <?php if(!empty($heading)): ?>
+            <h3 class="be-icon-box-block--heading"> 
+                <?php if(!empty($link)){ ?>
+                    <a href="<?php echo $link ?>">  <?php echo $heading ?>  </a>
+                <?php }else{
+                    echo $heading;
+                } ?>
+            </h3>
+        <?php endif; ?>    
+    </div> 
+
+    <div class="be-icon-box-block-hover"> 
+        <div class="be-icon-box-block-hover--inner"> 
             <?php if(!empty($heading)): ?>
-                <h3 class="be-icon-box-block--heading"> <?php echo $heading ?> </h3>
-            <?php endif; ?>    
+                <h3 class="be-icon-box-block--heading"> 
+                    <?php if(!empty($link)){ ?>
+                        <a href="<?php echo $link ?>">  <?php echo $heading ?>  </a>
+                    <?php }else{
+                        echo $heading;
+                    } ?>
+                </h3>
+            <?php endif; ?>  
+
+            <?php if(!empty($button['text']) && !empty($button['link'])): ?>
+                <div class="be-icon-box-block--button"> 
+                    <a href="<?php echo $button['link'] ?>"> <?php echo $button['text'] ?> </a>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+<?php }
+
+function be_template_icon_box_default($heading, $icon, $button, $link){ ?>
+    <div class="be-icon-box-block-warp"> 
+        <div class="be-icon-box-block--icon"> 
+            <img src="<?php echo $icon ?>" alt="icon">
+        </div>
+
+        <?php if(!empty($heading)): ?>
+            <h3 class="be-icon-box-block--heading"> 
+                <?php if(!empty($link)){ ?>
+                    <a href="<?php echo $link ?>">  <?php echo $heading ?>  </a>
+                <?php }else{
+                    echo $heading;
+                } ?>
+            </h3>
+        <?php endif; ?>    
     </div> 
 
     <div class="be-icon-box-block-hover"> 
--- resources/blocks/be-icon-box/render.php
+++ resources/blocks/be-icon-box/render.php
@@ -7,16 +7,13 @@ $id = 'be-icon-box-' . $block['id'];
 $align_class = $block['align'] ? 'align' . $block['align'] : '';
 
 // ACF field variables
-$heading = get_field('heading_icon_box');
-$content = get_field('content_icon_box');
-$icon    = get_field('icon__icon_box');
-$button  = get_field('button_icon_box');
+$bgImage = (!empty(get_field('bg__icon_box'))) ? get_field('bg__icon_box') : '';
 
 $is_style = isset($block['className']) ? $block['className'] : "is-style-default";
 
 ?>
 <div id="<?php echo $id; ?>" class="be-icon-box-block <?php echo $align_class; ?> <?php echo $is_style?>"> 
-   <div class="be-icon-box-block-inner"> 
+   <div class="be-icon-box-block-inner" style="background-image: url('<?php echo $bgImage ?>')"> 
         <?php be_item_icon_box($block) ?>
    </div>
 </div>
