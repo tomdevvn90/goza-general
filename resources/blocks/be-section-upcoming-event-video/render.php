de5bb5c1a592b58475ec21955278a2223a5af2f8
tomdevvn90 <tomdevvn90@gmail.com>
2023-04-09 23:24:24 +0700

    update code

--- resources/blocks/be-section-upcoming-event-video/render.php
+++ resources/blocks/be-section-upcoming-event-video/render.php
@@ -13,7 +13,8 @@ $desc     = (!empty($general['description'])) ? $general['description'] : '';
 $bg       = (!empty($general['bg_img'])) ? $general['bg_img'] : '';
 $bg_left  = (!empty($bg['left'])) ? $bg['left'] : get_template_directory_uri(). '/resources/assets/images/bg-event-left-default.jpg';
 $bg_right = (!empty($bg['right'])) ? $bg['right'] : get_template_directory_uri(). '/resources/assets/images/bg-event-right-default.jpg';
-
+$show_vd  = (!empty($general['show_video'])) ? $general['show_video'] : false;
+$url_vd   = (!empty($general['url_video'])) ? $general['url_video'] : '';
 ?>
 <div id="<?php echo $id; ?>" class="be-ss-upcoming-event-video <?php echo $align_class; ?>"> 
    <div class="be-ss-upcoming-event-video--bg"> 
@@ -32,6 +33,14 @@ $bg_right = (!empty($bg['right'])) ? $bg['right'] : get_template_directory_uri()
             <?php endif; ?> 
 
             <?php be_events_listing() ?>
+
+            <?php if($show_vd && !empty($url_vd)): ?>
+               <div class="be-ss-upcoming-event-video--content-cta"> 
+                  <a href="<?php echo $url_vd ?>">
+                     <span class="__icon-play"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="512" height="512" x="0" y="0" viewBox="0 0 163.861 163.861" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M34.857 3.613C20.084-4.861 8.107 2.081 8.107 19.106v125.637c0 17.042 11.977 23.975 26.75 15.509L144.67 97.275c14.778-8.477 14.778-22.211 0-30.686L34.857 3.613z" fill="#000000" data-original="#000000" class=""></path></g></svg></span>
+                  </a>
+               </div>
+            <?php endif; ?>   
         </div>
    </div>
 </div>
