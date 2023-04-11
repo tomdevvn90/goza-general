dc2d33252a2fc52e08223ab6b487e8572947b82e
tomdevvn90 <tomdevvn90@gmail.com>
2023-04-11 15:48:29 +0700

    update Counter

--- resources/blocks/be-numberal-donation-box/render.php
+++ resources/blocks/be-numberal-donation-box/render.php
@@ -7,12 +7,13 @@ $id = 'be-numberal-donation-box-' . $block['id'];
 $align_class = $block['align'] ? 'align' . $block['align'] : '';
 
 // ACF field variables
-$general     = (!empty(get_field('general_numberal_donation_box'))) ? get_field('general_numberal_donation_box') : '';
-$counter     = (!empty(get_field('counter_numberal_donation_box'))) ? get_field('counter_numberal_donation_box') : '';
-$list_items  = (!empty($counter['list_items'])) ? $counter['list_items'] : '';
-$button_one  = (!empty($general['button_one'])) ? $general['button_one'] : '';
-$button_two  = (!empty($general['button_one'])) ? $general['button_one'] : '';
-$bg          = (!empty($general['bg_image'])) ? $general['bg_image'] : get_template_directory_uri(). '/resources/assets/images/bg-number-box-default.jpg';
+$general       = (!empty(get_field('general_numberal_donation_box'))) ? get_field('general_numberal_donation_box') : '';
+$counter       = (!empty(get_field('counter_numberal_donation_box'))) ? get_field('counter_numberal_donation_box') : '';
+$list_items    = (!empty($counter['list_items'])) ? $counter['list_items'] : '';
+$button_one    = (!empty($general['button_one'])) ? $general['button_one'] : '';
+$button_two    = (!empty($general['button_one'])) ? $general['button_one'] : '';
+$btn_animation = (!empty($general['animation_button'])) ? $general['animation_button'] : '';
+$bg            = (!empty($general['bg_image'])) ? $general['bg_image'] : get_template_directory_uri(). '/resources/assets/images/bg-number-box-default.jpg';
 ?>
 <div id="<?php echo $id; ?>" class="be-numberal-donation-box <?php echo $align_class; ?>" style="background-image:url('<?php echo $bg ?>')"> 
   <div class="be-numberal-donation-box-inner">
@@ -21,22 +22,17 @@ $bg          = (!empty($general['bg_image'])) ? $general['bg_image'] : get_templ
             <?php 
                $st_counter = (!empty($counter['settings'])) ? $counter['settings'] : '';
                $duration   = (!empty($st_counter['duration'])) ?  $st_counter['duration'] : 1000;
-               $offset     = (!empty($st_counter['offset'])) ?  $st_counter['offset'] : 50;
                $delay      = (!empty($st_counter['delay'])) ?  $st_counter['delay'] : 800;
             ?>
             <?php foreach ($list_items as $key => $value): ?>
                <?php 
-                  $delay_item = (($key * 200) + $delay)
+                  $delay_item = (($key * 20) + $delay)
                ?>
                <div class="item-numberal-donation"> 
                      <div class="item-numberal-donation--number"> 
                         <?php if(!empty($value['numberal'])): ?>
-                           <span data-counter data-offset="<?php echo $offset ?>" data-duration="<?php echo $duration ?>" data-delay="<?php echo $delay_item ?>"> <?php echo $value['numberal'] ?></span>
-                        <?php endif; ?>
-
-                        <?php if(!empty($value['after_prefix'])): ?>
-                           <span class="item-numberal-donation--numberal-after"> <?php echo $value['after_prefix'] ?> </span>
-                        <?php endif; ?>   
+                           <span data-counter  data-duration="<?php echo $duration ?>" data-delay="<?php echo $delay_item ?>"> <?php echo $value['numberal'] ?></span>
+                        <?php endif; ?>  
                      </div>
 
                      <?php if(!empty($value['heading'])): ?>
@@ -48,10 +44,29 @@ $bg          = (!empty($general['bg_image'])) ? $general['bg_image'] : get_templ
          </div>
       <?php endif; ?>   
 
-
       <div class="be-numberal-donation-box--buttons"> 
-         <?php if(!empty($button_one)): ?>
+         <?php if(!empty($button_one['name']) && !empty($button_one['link'])): ?>
+            <div class="be-numberal-donation-box--buttons-one be-button be-button-<?php echo $btn_animation ?>"> 
+               <a href="<?php echo $button_one['link'] ?>"> 
+                     <?php if(!empty($btn_animation === 'style_2')): ?>
+                        <svg class="wgl-dashes inner-dashed-border animated-dashes"> <rect rx="0%" ry="0%">  </rect> </svg>
+                     <?php endif; ?>    
+                     <?php echo $button_one['name']; ?>
+               </a>
+            </div>
          <?php endif; ?>   
+
+         <?php if(!empty($button_two['name']) && !empty($button_two['link'])): ?>
+            <div class="be-numberal-donation-box--buttons-two be-button be-button-<?php echo $btn_animation ?>"> 
+               <a href="<?php echo $button_two['link'] ?>"> 
+                     <?php if(!empty($btn_animation === 'style_2')): ?>
+                        <svg class="wgl-dashes inner-dashed-border animated-dashes"> <rect rx="0%" ry="0%">  </rect> </svg>
+                     <?php endif; ?>    
+                     <?php echo $button_two['name']; ?>
+               </a>
+            </div>
+         <?php endif; ?>   
+
       </div>
   </div>
 </div>
