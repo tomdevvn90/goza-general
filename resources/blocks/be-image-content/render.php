0f996e71c8b93a59f9a0cb3332ca10e77dd14667
Rim <bvt.vanthang2010@gmail.com>
2023-05-02 22:08:39 +0700

    update image content block

--- resources/blocks/be-image-content/render.php
+++ resources/blocks/be-image-content/render.php
@@ -38,11 +38,13 @@ $icb_avatar_author      = __get_field('icb_avatar_author') ?: '';
 $icb_quote_text         = __get_field('icb_quote_text') ?: '';
 $icb_name_author        = __get_field('icb_name_author') ?: '';
 $icb_position_author    = __get_field('icb_position_author') ?: '';
-$icb_headingcolor          = __get_field('icb_textcolor') ?: '';
+$icb_headingcolor       = __get_field('icb_textcolor') ?: '';
 $icb_bgcolor            = __get_field('icb_bgcolor') ?: '';
 $icb_author_color       = __get_field('icb_author_color') ?: '';
 $icb_quote_color        = __get_field('icb_quote_color') ?: '';
 $icb_desc_color         = __get_field('icb_desc_color') ?: '';
+$icb_subheading_color   = __get_field('icb_subheading_color') ?: '';
+$icb_signature_author   = __get_field('icb_signature_author') ?: '';
 
 // Build a valid style attribute for background colors.
 $styles = array('background-color: ' . $icb_bgcolor);
@@ -51,12 +53,12 @@ $style  = implode('; ', $styles);
 ?>
 <div <?php echo $anchor; ?>class="<?php echo esc_attr($class_name); ?>" style="<?php echo esc_attr($style); ?>">
    <div class="block-inner">
-      <div class="block-inner__col block-inner__image"  data-aos="zoom-in">
+      <div class="block-inner__col block-inner__image" data-aos="zoom-in">
          <img src="<?= ($icb_image) ? esc_url($icb_image['url']) : 'https://via.placeholder.com/900x600?text=Image' ?>" alt="<?= ($icb_image) ? esc_attr($icb_image['alt']) : '' ?>" />
       </div>
       <div class="block-inner__col block-inner__content">
          <?php if ($icb_subheading) { ?>
-            <h5 class="block-inner__content-subheading" data-aos="fade-up" style="color: <?= $icb_headingcolor ?>"><?= esc_attr($icb_subheading) ?></h5>
+            <h5 class="block-inner__content-subheading" data-aos="fade-up" style="color: <?= $icb_subheading_color ?>"><?= esc_attr($icb_subheading) ?></h5>
          <?php } ?>
 
          <?php if ($icb_heading) { ?>
@@ -71,19 +73,27 @@ $style  = implode('; ', $styles);
             <div class="block-inner__content-quote-text" data-aos="fade-up" style="color: <?= $icb_quote_color ?>"><?= esc_attr($icb_quote_text) ?></div>
          <?php } ?>
 
-         <div class="block-inner__content-quote-author">
-            <div class="quote-author-avatar" data-aos="zoom-in">
-               <img src="<?= ($icb_avatar_author) ? esc_url($icb_avatar_author['url']) : 'https://via.placeholder.com/80x80?text=Avatar' ?>" alt="<?= esc_attr($icb_name_author) ?>" />
+         <div class="block-inner__content-bottom">
+            <div class="block-inner__content-quote-author">
+               <div class="quote-author-avatar" data-aos="zoom-in">
+                  <img src="<?= ($icb_avatar_author) ? esc_url($icb_avatar_author['url']) : 'https://via.placeholder.com/80x80?text=Avatar' ?>" alt="<?= esc_attr($icb_name_author) ?>" />
+               </div>
+               <div class="quote-author-name-pos" data-aos="zoom-in">
+                  <?php if ($icb_name_author) { ?>
+                     <div class="quote-author-name" style="color: <?= $icb_author_color ?>"><?= esc_attr($icb_name_author) ?></div>
+                  <?php } ?>
+                  <?php if ($icb_position_author) { ?>
+                     <div class="quote-author-position" style="color: <?= $icb_author_color ?>"><?= esc_attr($icb_position_author) ?></div>
+                  <?php } ?>
+               </div>
             </div>
-            <div class="quote-author-name-pos" data-aos="fade-up">
-               <?php if ($icb_name_author) { ?>
-                  <div class="quote-author-name" style="color: <?= $icb_author_color ?>"><?= esc_attr($icb_name_author) ?></div>
-               <?php } ?>
-               <?php if ($icb_position_author) { ?>
-                  <div class="quote-author-position" style="color: <?= $icb_author_color ?>"><?= esc_attr($icb_position_author) ?></div>
-               <?php } ?>
-            </div>
+            <?php if ($icb_signature_author) { ?>
+               <div class="block-inner__content-signature"  data-aos="zoom-in">
+                  <img src="<?= esc_url($icb_signature_author['url']) ?>" alt="signature" />
+               </div>
+            <?php } ?>
          </div>
+
       </div>
    </div>
 </div>
