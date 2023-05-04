ea4daf00979af2d117137d32c6573bdbd18c70a4
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-04 12:01:48 +0700

    update option style hero ss

--- resources/blocks/be-section-hero/render.php
+++ resources/blocks/be-section-hero/render.php
@@ -3,28 +3,29 @@
 // create id attribute for specific styling
 $id = 'be-ss-hero-'.$block['id'];
 
-// create align class ("alignwide") from block setting ("wide")
-$align_class = $block['align'] ? 'align' . $block['align'] : '';
-
 $bg = get_field('bg_ss_hero');
 $bg_color = get_field('bg_color_ss_hero');
 $bg_overlay_color = get_field('bg_overlay_color_ss_hero');
 $bg_overlay_opacity = get_field('bg_overlay_opacity_ss_hero');
+$content_alignmnet = get_field('content_alignmnet_ss_hero');
 $icon = get_field('icon_ss_hero');
 $heading = get_field('heading_ss_hero');
 $heading_color = get_field('heading_color_ss_hero');
 $breadcrumb_color = get_field('breadcrumb_color_ss_hero');
 
-$bg_image_style = !empty( $bg['url'] )? 'background-image: url('.$bg['url'].')' : '';
-$bg_color_style = !empty( $bg_color )? 'background-color:'.$bg_color : '';
-$heading_color_style = !empty( $heading_color )? 'color:'.$heading_color : '';
-$breadcrumb_color_style = !empty( $breadcrumb_color )? 'color:'.$breadcrumb_color : '';
+$bg_image_style = !empty( $bg['url'] )? 'background-image: url('.$bg['url'].');' : '';
+$bg_color_style = !empty( $bg_color )? 'background-color:'.$bg_color.';' : '';
+$bg_overlay_color_style = !empty( $bg['url'] )? 'background-color:'.$bg_overlay_color.';' : '';
+$bg_overlay_opacity_style = !empty( $bg_color )? 'opacity:'.$bg_overlay_opacity.';' : '';
+$content_alignmnet_style = 'text-align:'.$content_alignmnet.';' ;
+$heading_color_style = !empty( $heading_color )? 'color:'.$heading_color.';' : '';
+$breadcrumb_color_style = !empty( $breadcrumb_color )? 'color:'.$breadcrumb_color.';' : '';
 
 ?>
 <section id="<?php echo $id ?>" class="be-ss-hero" style="<?php echo $bg_image_style; ?> <?php echo $bg_color_style; ?>">
-    <div class="be-ss-hero--bg-overlay" style="<?php echo $bg_overlay_color; ?><?php echo $bg_overlay_opacity; ?>"></div>
+    <div class="be-ss-hero--bg-overlay" style="<?php echo $bg_overlay_color_style; ?><?php echo $bg_overlay_opacity_style; ?>"></div>
     <div class="be-ss-hero--content container">
-        <div class="be-ss-hero-inner <?php echo $align_class; ?>">
+        <div class="be-ss-hero-inner" style="<?php echo $content_alignmnet_style; ?>">
 
             <?php if ( !empty( $icon ) ): ?>
                 <div class="be-ss-hero-inner__icon">
