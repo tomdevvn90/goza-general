75387a7466b0e6f86951e65b1c77bb955536a710
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-04 10:18:28 +0700

    update hero section

--- resources/blocks/be-section-hero/render.php
+++ resources/blocks/be-section-hero/render.php
@@ -13,13 +13,17 @@ $heading = get_field('heading_ss_hero');
 $heading_color = get_field('heading_color_ss_hero');
 $breadcrumb_color = get_field('breadcrumb_color_ss_hero');
 
+$bg_color_style = !empty( $bg_color )? 'background-color:'.$bg_color : '';
+$heading_color_style = !empty( $heading_color )? 'color:'.$heading_color : '';
+$breadcrumb_color_style = !empty( $breadcrumb_color )? 'color:'.$breadcrumb_color : '';
+
 ?>
-<section id="<?php echo $id ?>" class="be-ss-hero <?php echo $align_class; ?>">
+<section id="<?php echo $id ?>" class="be-ss-hero" style="<?php echo $bg_color_style; ?>">
     <div class="be-ss-hero--bg"> 
       <img src="<?php echo esc_url( $bg['url'] ); ?>" alt="<?php echo esc_attr( $bg['alt'] ); ?>">
     </div>
     <div class="be-ss-hero--content container">
-        <div class="be-ss-hero--inner">
+        <div class="be-ss-hero--inner <?php echo $align_class; ?>">
 
             <?php if ( !empty( $icon ) ): ?>
                 <div class="be-ss-hero--icon">
@@ -28,11 +32,11 @@ $breadcrumb_color = get_field('breadcrumb_color_ss_hero');
             <?php endif; ?>   
             
             <?php if ( !empty( $heading ) ): ?>
-                <h2 class="be-ss-hero--heading"><?php echo $heading; ?></h2>
+                <h2 class="be-ss-hero--heading" style="<?php echo $heading_color_style; ?>"><?php echo $heading; ?></h2>
             <?php endif; ?>   
          
             <?php if ( function_exists( 'yoast_breadcrumb' ) ): ?>
-                <div class="be-ss-hero--breadcrumb">
+                <div class="be-ss-hero--breadcrumb" style="<?php echo $breadcrumb_color_style; ?>">
                     <?php yoast_breadcrumb(); ?>  
                 </div> 
             <?php endif; ?>   
