590f423143e4ee19a097c091bb7e630c7d7f3bdf
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-04 13:25:50 +0700

    update style option

--- resources/blocks/be-section-hero/render.php
+++ resources/blocks/be-section-hero/render.php
@@ -1,8 +1,8 @@
 <?php
-
 // create id attribute for specific styling
 $id = 'be-ss-hero-'.$block['id'];
 
+$min_height = get_field('min_height_ss_hero');
 $bg = get_field('bg_ss_hero');
 $bg_color = get_field('bg_color_ss_hero');
 $bg_overlay_color = get_field('bg_overlay_color_ss_hero');
@@ -13,10 +13,11 @@ $heading = get_field('heading_ss_hero');
 $heading_color = get_field('heading_color_ss_hero');
 $breadcrumb_color = get_field('breadcrumb_color_ss_hero');
 
+$bg_image_style = !empty( $min_height )? 'min-height: '.$min_height.';' : '';
 $bg_image_style = !empty( $bg['url'] )? 'background-image: url('.$bg['url'].');' : '';
 $bg_color_style = !empty( $bg_color )? 'background-color:'.$bg_color.';' : '';
-$bg_overlay_color_style = !empty( $bg['url'] )? 'background-color:'.$bg_overlay_color.';' : '';
-$bg_overlay_opacity_style = !empty( $bg_color )? 'opacity:'.$bg_overlay_opacity.';' : '';
+$bg_overlay_color_style = !empty( $bg_overlay_color )? 'background-color:'.$bg_overlay_color.';' : '';
+$bg_overlay_opacity_style = !empty( $bg_overlay_opacity )? 'opacity:'.$bg_overlay_opacity.';' : '';
 $content_alignmnet_style = 'text-align:'.$content_alignmnet.';' ;
 $heading_color_style = !empty( $heading_color )? 'color:'.$heading_color.';' : '';
 $breadcrumb_color_style = !empty( $breadcrumb_color )? 'color:'.$breadcrumb_color.';' : '';
