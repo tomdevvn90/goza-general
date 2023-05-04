97ad83c1fdb60dc0c258fa5ac6884ec9fe244aa3
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-04 11:18:53 +0700

    update bg overlay hero section

--- resources/blocks/be-section-hero/render.php
+++ resources/blocks/be-section-hero/render.php
@@ -8,20 +8,21 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
 
 $bg = get_field('bg_ss_hero');
 $bg_color = get_field('bg_color_ss_hero');
+$bg_overlay_color = get_field('bg_overlay_color_ss_hero');
+$bg_overlay_opacity = get_field('bg_overlay_opacity_ss_hero');
 $icon = get_field('icon_ss_hero');
 $heading = get_field('heading_ss_hero');
 $heading_color = get_field('heading_color_ss_hero');
 $breadcrumb_color = get_field('breadcrumb_color_ss_hero');
 
+$bg_image_style = !empty( $bg['url'] )? 'background-image: url('.$bg['url'].')' : '';
 $bg_color_style = !empty( $bg_color )? 'background-color:'.$bg_color : '';
 $heading_color_style = !empty( $heading_color )? 'color:'.$heading_color : '';
 $breadcrumb_color_style = !empty( $breadcrumb_color )? 'color:'.$breadcrumb_color : '';
 
 ?>
-<section id="<?php echo $id ?>" class="be-ss-hero" style="<?php echo $bg_color_style; ?>">
-    <div class="be-ss-hero--bg"> 
-      <img src="<?php echo esc_url( $bg['url'] ); ?>" alt="<?php echo esc_attr( $bg['alt'] ); ?>">
-    </div>
+<section id="<?php echo $id ?>" class="be-ss-hero" style="<?php echo $bg_image_style; ?> <?php echo $bg_color_style; ?>">
+    <div class="be-ss-hero--bg-overlay" style="<?php echo $bg_overlay_color; ?><?php echo $bg_overlay_opacity; ?>"></div>
     <div class="be-ss-hero--content container">
         <div class="be-ss-hero-inner <?php echo $align_class; ?>">
 
