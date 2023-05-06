87f5f657142486021b8470e03f4fafbe059fdde7
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-06 11:49:46 +0700

    update block google map

--- resources/blocks/be-google-map/render.php
+++ resources/blocks/be-google-map/render.php
@@ -5,25 +5,21 @@ $id = 'be-ss-hero-'.$block['id'];
 $align_class = $block['align'] ? 'align' . $block['align'] : '';
 
 $location = get_field('location_google_map_block');
-$zoom = get_field('zoom_google_map_block');
+$height = get_field('height_google_map_block');
 
-$height = get_field('height_google_map_block')? : 500;
-
-$url = 'https://maps.google.com/maps?q=%1$s&amp;t=m&amp;z=%2$d&amp;output=embed&amp;iwloc=near';
-$params = [
-    rawurlencode( $location ),
-    absint( $zoom ),
-];
+$height_style = !empty( $height )? '--google-map-height'.$height.'px;' : '';
 
 ?>
 <section id="<?php echo $id; ?>" class="be-google-map <?php echo $align_class; ?>">
     <div class="be-google-map--content">
-        <div class="be-google-map-box">
-            <iframe height="<?php echo $height; ?>" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen
-                    src="<?php echo esc_url( vsprintf( $url, $params ) ); ?>"
-                    title="<?php echo esc_attr( $location ); ?>"
-                    aria-label="<?php echo esc_attr( $location ); ?>"
-            ></iframe>
+        <div class="be-google-map-box" style="<?php echo $height_style; ?>">
+            <?php
+            if ( !empty( $location ) ) {
+               echo $location;
+            }else{
+                echo '<p>Please Embed Google Map!</p>';
+            }
+            ?>
         </div>
     </div>
 </section>
