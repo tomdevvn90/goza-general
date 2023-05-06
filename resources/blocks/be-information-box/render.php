dd51aa3c9af8bc0e395f35c245f6f68ddf94c867
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-06 10:53:55 +0700

    update style contact form 7

--- resources/blocks/be-information-box/render.php
+++ resources/blocks/be-information-box/render.php
@@ -23,7 +23,7 @@ $data_aos_animated = ($info_animation != 'none')? $info_animation : '';
 
 ?>
 <section id="<?php echo $id; ?>" class="be-infomation-section <?php echo $align_class; ?>">
-    <div class="be-infomation-box container">
+    <div class="be-infomation-box">
         <div data-aos="<?php echo $data_aos_animated; ?>" class="be-infomation-list <?php echo $shadow_class; ?>" style="<?php echo $info_bg_color_style; ?><?php echo $info_margin_top_style; ?>">
             <?php if ( !empty( $infomation_list ) ) {
                 
