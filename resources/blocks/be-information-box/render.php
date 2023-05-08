d91c57fc07fef8f6f44b85b865c9ed7b25b671a3
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-05-08 09:34:31 +0700

    remove heading block infomation box

--- resources/blocks/be-information-box/render.php
+++ resources/blocks/be-information-box/render.php
@@ -7,14 +7,12 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
 $infomation_list = get_field('infomation_list_block');
 
 $info_bg_color = get_field('infomation_bg_color_block');
-$info_heading_color = get_field('heading_color_infomation_block');
 $info_text_color = get_field('text_color_infomation_block');
 $info_margin_top = get_field('margin_top_infomation_block');
 $info_box_shadow = get_field('box_shadow_infomation_block');
 $info_animation = get_field('animation_infomation_block');
 
 $info_bg_color_style = !empty($info_bg_color)? 'background-color:'.$info_bg_color.';' : '';
-$info_heading_color_style = !empty($info_heading_color)? 'color:'.$info_heading_color.';' : '';
 $info_text_color_style = !empty($info_text_color)? 'color:'.$info_text_color.';' : '';
 $info_margin_top_style = !empty($info_margin_top)? 'margin-top:'.$info_margin_top.'px;' : '';
 
@@ -36,16 +34,11 @@ $data_aos_animated = ($info_animation != 'none')? $info_animation : '';
                     <div class="be-infmation-item">
                         <?php if( !empty( $value['icon']['url'] ) ): ?>
                         <div class="be-infmation-item--icon-wrap">
-                            <img src="<?php echo $value['icon']['url']; ?>" alt="<?php echo $value['icon']['alt'] ?>">
+                            <img src="<?php echo esc_url( $value['icon']['url']); ?>" alt="<?php echo esc_attr( $value['icon']['alt'] ); ?>">
                         </div>
                         <?php endif; ?>
-                        <?php if( !empty( $value['heading'] ) || !empty( $value['text'] ) || !empty( $phone_list ) || !empty( $email_list ) ): ?>
+                        <?php if( !empty( $value['text'] ) || !empty( $phone_list ) || !empty( $email_list ) ): ?>
                         <div class="be-infmation-item--content-wrap">
-                            <?php if( !empty( $value['heading'] ) ): ?>
-                                <h4 class="be-infmation-item--heading" style="<?php echo $info_heading_color_style; ?>">
-                                    <?php echo $value['heading']; ?>
-                                </h4>
-                            <?php endif; ?>
                             <?php if( $field_type == 'text' && !empty( $value['text'] ) ): ?>
                                 <div class="be-infmation-item--text" style="<?php echo $info_text_color_style; ?>">
                                     <?php echo $value['text']; ?>
@@ -59,7 +52,7 @@ $data_aos_animated = ($info_animation != 'none')? $info_animation : '';
                                         
                                         foreach ($phone_list as $key => $phone) {
                                             $format_phone = preg_replace('/[^A-Za-z0-9]/', '', $phone['phone_number']);
-                                            echo '<a href="tel:'.$format_phone.'">'.$phone['phone_number'].'</a>';
+                                            echo '<a href="tel:'.esc_attr( $format_phone ).'">'.$phone['phone_number'].'</a>';
                                             if ( $i != $total - 1 ) {
                                                echo '<br>';
                                             }
@@ -75,7 +68,7 @@ $data_aos_animated = ($info_animation != 'none')? $info_animation : '';
                                         $total = count( $email_list );
 
                                         foreach ($email_list as $key => $email) {
-                                            echo '<a href="mailto:'.$email['email_address'].'">'.$email['email_address'].'</a>';
+                                            echo '<a href="mailto:'.esc_attr( $email['email_address'] ).'">'.$email['email_address'].'</a>';
                                             if ( $i != $total - 1 ) {
                                                echo '<br>';
                                             }
