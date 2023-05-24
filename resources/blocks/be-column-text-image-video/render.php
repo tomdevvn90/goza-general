f6174941852fd990d0cfb15fc36de69ebc7db41f
Rim <bvt.vanthang2010@gmail.com>
2023-05-24 22:13:30 +0700

    update col image text video block

--- resources/blocks/be-column-text-image-video/render.php
+++ resources/blocks/be-column-text-image-video/render.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Box Network Connect Block Template.
+ * Be Column Image Text Video Block Template.
  *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
@@ -19,7 +19,7 @@ if (!empty($block['anchor'])) {
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'box-network-connect-block';
+$class_name = 'be-column-image-text-video-block';
 if (!empty($block['className'])) {
    $class_name .= ' ' . $block['className'];
 }
@@ -28,15 +28,21 @@ if (!empty($block['align'])) {
 }
 
 // Load values and assign defaults.
-$bnc_icon            = __get_field('bnc_icon') ?: '';
-$bnc_title           = __get_field('bnc_title') ?: '';
-$bnc_desc            = __get_field('bnc_desc') ?: '';
-$bnc_cta             = __get_field('bnc_cta') ?: [];
-$bnc_bg_img          = __get_field('bnc_bg_img');
-$bnc_bg_color        = __get_field('bnc_bg_color') ?: '';
-$bnc_bg_color_box    = __get_field('bnc_bg_color_box') ?: '';
-$bnc_txt_color       = __get_field('bnc_txt_color') ?: '';
-$bnc_style_button    = __get_field('bnc_style_button') ?: 'btn-default';
+$citv_heading              = __get_field('citv_heading') ?: '';
+$citv_subheading           = __get_field('citv_subheading') ?: '';
+$citv_button               = __get_field('citv_button') ?: '';
+$citv_description          = __get_field('citv_description') ?: '';
+$citv_name_person          = __get_field('citv_name_person');
+$citv_position_person      = __get_field('citv_position_person') ?: '';
+$citv_type_video           = __get_field('citv_type_video') ?: '';
+$citv_link_video           = __get_field('citv_link_video') ?: '';
+$citv_link_video_local     = __get_field('citv_link_video_local') ?: '';
+$citv_bg_img_content       = __get_field('citv_bg_img_content') ?: '';
+$citv_bg_img_video         = __get_field('citv_bg_img_video') ?: '';
+$citv_subheading_color     = __get_field('citv_subheading_color') ?: '';
+$citv_heading_color        = __get_field('citv_heading_color') ?: '';
+$citv_desc_color           = __get_field('citv_desc_color') ?: '';
+$citv_button_style          = __get_field('citv_button_style') ?: 'btn-default';
 
 // Build a valid style attribute for background and text colors.
 $styles = array('background-color: ' . $bnc_bg_color);
@@ -47,31 +53,71 @@ $style  = implode('; ', $styles);
 
 ?>
 <div <?php echo $anchor; ?>class="<?php echo esc_attr($class_name); ?>" style="<?php echo esc_attr($style); ?>">
-   <div class="container">
-      <div class="block-box">
-         <?php if ($bnc_icon) { ?>
-            <img src="<?= esc_attr($bnc_icon['url']) ?>" alt="<?= esc_attr($bnc_icon['alt']) ?>" />
-         <?php } ?>
+      <div class="block-grid">
 
-         <?php if ($bnc_title) { ?>
-            <h4 class="bnc-title" style="color: <?= esc_attr($bnc_txt_color) ?>"><?= esc_attr($bnc_title) ?></h4>
-         <?php } ?>
-
-         <?php if ($bnc_desc) { ?>
-            <p class="bnc-description" style="color: <?= esc_attr($bnc_txt_color) ?>"><?= esc_attr($bnc_desc) ?></p>
-         <?php } ?>
-
-         <?php if ($bnc_cta) { ?>
-            <a href="<?= esc_url($bnc_cta['url']) ?>" target="<?= esc_attr($bnc_cta['target']) ?>" class="btn <?= esc_attr($bnc_style_button) ?>">
-               <?= esc_attr($bnc_cta['title']) ?>
-               <?php if ($bnc_style_button == 'btn-water') { ?>
-                  <svg class="wgl-dashes inner-dashed-border animated-dashes">
-                     <rect rx="0%" ry="0%"> </rect>
-                  </svg>
+         <div class="block-col block-col-content">
+            <div class="block-col__bg">
+               <?php if (isset($citv_bg_img_content) && $citv_bg_img_content) { ?>
+                  <img src="<?= esc_url($citv_bg_img_content['url']) ?>" alt="<?= esc_attr($citv_bg_img_content['alt']) ?>" />
                <?php } ?>
-            </a>
-         <?php } ?>
+            </div>
+            <div class="block-col__content">
+               <?php if (isset($citv_subheading) && !empty($citv_subheading)) { ?>
+                  <h6 class="block-col__subheading"><?= esc_attr($citv_subheading) ?></h6>
+               <?php } ?>
 
+               <?php if (isset($citv_heading) && !empty($citv_heading)) { ?>
+                  <h2 class="block-col__heading"><?= esc_attr($citv_heading) ?></h2>
+               <?php } ?>
+
+               <?php if (isset($citv_description) && !empty($citv_description)) { ?>
+                  <div class="block-col__desc"><?= $citv_description ?></div>
+               <?php } ?>
+
+               <?php if (isset($citv_button) && $citv_button) { ?>
+                  <a href="<?= esc_url($citv_button['url']) ?>" target="<?= esc_attr($citv_button['target']) ?>" class="btn <?= esc_attr($citv_button_style) ?>">
+                     <?= esc_attr($citv_button['title']) ?>
+                     <?php if ($citv_button_style == 'btn-water') { ?>
+                        <svg class="wgl-dashes inner-dashed-border animated-dashes">
+                           <rect rx="0%" ry="0%"> </rect>
+                        </svg>
+                     <?php } ?>
+                  </a>
+               <?php } ?>
+            </div>
+         </div>
+
+         <div class="block-col block-col-video">
+            <div class="block-col__bg">
+               <?php if (isset($citv_bg_img_video) && $citv_bg_img_video) { ?>
+                  <img src="<?= esc_url($citv_bg_img_video['url']) ?>" alt="<?= esc_attr($citv_bg_img_video['alt']) ?>" />
+               <?php } ?>
+
+               <div class="block-col__box">
+                  <div class="block-col__box-content">
+                     <?php if (isset($citv_name_person) && !empty($citv_name_person)) { ?>
+                        <h5 class="block-col__name"><?= esc_attr($citv_name_person) ?></h5>
+                     <?php } ?>
+                     <?php if (isset($citv_position_person) && !empty($citv_position_person)) { ?>
+                        <h5 class="block-col__position"><?= esc_attr($citv_position_person) ?></h5>
+                     <?php } ?>
+                  </div>
+                  <div class="block-col__box-action">
+                     <?php if ($citv_type_video == 'local') { ?>
+                        <?php $video_url = $citv_link_video_local ? $citv_link_video_local : ''; ?>
+                        <!-- HTML5 Video --->
+                        <a data-lg-size="1280-720" data-video='{"source": [{"src":"<?= esc_url($video_url) ?>", "type":"video/mp4"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}'>
+                           <i class="fa fa-play-circle-o"></i>
+                        </a>
+                     <?php } else { ?>
+                        <?php $video_url = $citv_link_video ? $citv_link_video : ''; ?>
+                        <a data-lg-size="1280-720" data-src="<?= esc_url($video_url) ?>">
+                           <i class="fa fa-play-circle-o"></i>
+                        </a>
+                     <?php } ?>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
-   </div>
 </div>
