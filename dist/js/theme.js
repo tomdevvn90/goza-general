1d363abb403a5019e8e45f0eafc38e65baaf7ce7
pidev077 <nguyenanhphi077@gmail.com>
2023-06-21 15:14:45 +0700

    skin iconbox and post slider

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -1256,9 +1256,9 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ADMIN\Local Sites\goza-theme-dev\app\public\wp-content\themes\goza-theme\resources\assets\js\theme.js */"./resources/assets/js/theme.js");
-__webpack_require__(/*! C:\Users\ADMIN\Local Sites\goza-theme-dev\app\public\wp-content\themes\goza-theme\resources\assets\scss\theme.scss */"./resources/assets/scss/theme.scss");
-module.exports = __webpack_require__(/*! C:\Users\ADMIN\Local Sites\goza-theme-dev\app\public\wp-content\themes\goza-theme\resources\assets\scss\editor\editor.scss */"./resources/assets/scss/editor/editor.scss");
+__webpack_require__(/*! C:\Users\Admin\Local Sites\goza\app\public\wp-content\themes\goza-theme\resources\assets\js\theme.js */"./resources/assets/js/theme.js");
+__webpack_require__(/*! C:\Users\Admin\Local Sites\goza\app\public\wp-content\themes\goza-theme\resources\assets\scss\theme.scss */"./resources/assets/scss/theme.scss");
+module.exports = __webpack_require__(/*! C:\Users\Admin\Local Sites\goza\app\public\wp-content\themes\goza-theme\resources\assets\scss\editor\editor.scss */"./resources/assets/scss/editor/editor.scss");
 
 
 /***/ })
--- resources/blocks/be-icon-box/init.php
+++ resources/blocks/be-icon-box/init.php
@@ -7,6 +7,7 @@ function be_item_icon_box($block){
     $link    = get_field('link_icon_box');
     $icon    = (!empty(get_field('icon__icon_box'))) ? get_field('icon__icon_box') : get_template_directory_uri(). '/resources/assets/images/icon-box-default.svg' ;
     $button  = get_field('button_icon_box');
+    $subheading  = get_field('subheading_icon_box');
    
     switch ($is_style) {
         case "is-style-2":
@@ -15,13 +16,38 @@ function be_item_icon_box($block){
 
         case "is-style-3":
             be_template_icon_box_style_3($heading, $icon, $button, $link);
-            break; 
+            break;
+        
+        case "is-style-4":
+            be_template_icon_box_style_4($heading, $icon, $subheading, $link);
+            break;
 
         default:
             be_template_icon_box_default($heading, $icon, $button, $link);
             break; 
     } 
 }
+
+function be_template_icon_box_style_4($heading, $icon, $subheading, $link){ ?>
+    <div class="be-icon-box-block-inner--content"> 
+        <div class="be-icon-box-block--icon"> 
+            <img src="<?php echo esc_url($icon) ?>" alt="icon">
+        </div>
+
+        <?php if(!empty($heading)): ?>
+            <h3 class="be-icon-box-block--heading"> 
+                <?php if(!empty($link)){ ?>
+                    <a href="<?php echo esc_url($link) ?>">  <?php echo $heading ?>  </a>
+                <?php }else{
+                    echo $heading;
+                } ?>
+            </h3>
+        <?php endif; ?>  
+        <?php if(!empty($subheading)): ?>
+            <p class="be-icon-box-block--subheading"> <?php echo $subheading; ?> </p>
+        <?php endif; ?> 
+    </div>
+<?php }
 
 function be_template_icon_box_style_3($heading, $icon, $button, $link){ ?>
     <div class="be-icon-box-block-inner--content"> 
