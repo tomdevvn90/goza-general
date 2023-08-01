0cce6f78da814f856144f19abba7e2139fe5d9e1
Rim <bvt.vanthang2010@gmail.com>
2023-08-01 22:20:24 +0700

    update block count down

--- resources/assets/js/theme.js
+++ resources/assets/js/theme.js
@@ -1,10 +1,10 @@
 import 'slick-carousel';
 import counterUp from 'counterup2';
 
-// import './components/navigation';
-// import './components/function';
-// import './components/tabpanel';
-// import './components/woocommerce';
+import './components/navigation';
+import './components/function';
+import './components/tabpanel';
+import './components/woocommerce';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -21,26 +21,26 @@ AOS.init({
 $(document).ready(function () {
     const el = document.querySelector('.counterUp')
     if (el === null) return;
-    // counterUp(el, {
-    //     duration: 2000,
-    //     delay: 100,
-    // })
+    counterUp(el, {
+        duration: 2000,
+        delay: 100,
+    })
 });
 
-$(document).on("load", function () {
+$(document).on("load", function () { 
     AOS.refresh();
 })
 
-// import './blocks/progressbar-block';
-// import './blocks/team-carousel';
-// import './blocks/events-carousel';
-// import './blocks/testimonials';
-// import './blocks/posts-slider';
-// import './blocks/events-listing';
-// import './blocks/gives-slider';
-// import './blocks/video-popup-actions';
-// import './blocks/ss-upcoming-event-video';
-// import './blocks/ss-text-tsm-video';
-// import './blocks/logo-carousel';
-// import './blocks/projects-grid-action';
-// import './blocks/counter-box';
+import './blocks/progressbar-block';
+import './blocks/team-carousel';
+import './blocks/events-carousel';
+import './blocks/testimonials';
+import './blocks/posts-slider';
+import './blocks/events-listing';
+import './blocks/gives-slider';
+import './blocks/video-popup-actions';
+import './blocks/ss-upcoming-event-video';
+import './blocks/ss-text-tsm-video';
+import './blocks/logo-carousel';
+import './blocks/projects-grid-action';
+import './blocks/counter-box';
